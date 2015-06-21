637f92ea2402ef2afcc9286edb379947c71c6883
mariashka <bisyarinamariashka@gmail.com>
2015-06-22 01:39:43 +0300

    Fix fourth

--- task4.ml
+++ task4.ml
@@ -3,9 +3,17 @@ open Debruijn
 module D = Debruijn
 
 let _ =
-       	let s = read_line() in
-       	let e = Parser.lambda_expr Lexer.token (Lexing.from_string s) in
+	let s = read_line() in
+	let e = Parser.lambda_expr Lexer.token (Lexing.from_string s) in
 	let vars = ref [] in
-	let reduct = betareduct(convert e vars) in
+        let conv = convert e vars in
+	let str0 = to_string_debruijn conv in
+	(*print_endline (str0);*)
+	let str00 = to_string_lambda (convert_back conv !vars) in
+	let reduct = normalize conv in
+	let str1 = to_string_debruijn reduct in
 	let res = convert_back reduct !vars in
-	print_string (to_string_lambda res)
+	let str2 = to_string_lambda res in
+	(*print_endline (str1);*)
+	print_endline (str2)
+(* List.iter (Printf.printf "%s ") !vars ; *)
